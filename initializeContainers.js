4b19441a01b7c4a417db369a91b60f5b027c195e
Mustafa-Ashraf751 <mustafaashraf123@yahoo.com>
2025-02-03 19:32:37 +0200

    merge the styling with generate random blocks

--- initializeContainers.js
+++ initializeContainers.js
@@ -1,28 +1,34 @@
 export function initializeContainers(gameWidth, blockWidth, blockHeight) {
- 
   let blocks = [];
-  const spacing = 30; 
+  const randomRows = Math.floor(Math.random() * 3) + 4;
+  const spacings = [5, 10, 15, 20, 25];
+  const randomIndex = Math.floor(Math.random() * spacings.length);
+  const spacing = spacings[randomIndex];
   const colCount = Math.floor(gameWidth / (blockWidth + spacing));
-  const rowCount = 5;
+  const totalWidth = colCount * blockWidth + (colCount - 1) * spacing;
+  const randomHorizontal = [10, 20, 30, 50, 60, 70, 80];
+  const totalPageHeight = window.innerHeight;
+  const verticalMargin = 0.03 * totalPageHeight;
   let visibility;
-  for (let row = 0; row < rowCount; row++) {
+  for (let row = 0; row < randomRows; row++) {
+    const randomI = Math.floor(Math.random() * randomHorizontal.length);
+    const HorizontalMargin = randomHorizontal[randomI];
     for (let col = 0; col < colCount; col++) {
       if (Math.random() > 0.85) {
         visibility = -1;
-      }
-      else {
+      } else {
         visibility = 2;
-        }
-      let block ={  x: col * (blockWidth + spacing),
-      y: row * (blockHeight + spacing),
-      width: blockWidth,
-      height: blockHeight,
-      visible: visibility,
-      cracked:false,
-      inLevel:1,
-    }
-    blocks.push(block);
-    console.log(block.visible);    
+      }
+      let block = {
+        x: HorizontalMargin + col * (blockWidth + spacing),
+        y: verticalMargin + row * (blockHeight + spacing),
+        width: blockWidth,
+        height: blockHeight,
+        visible: visibility,
+        cracked: false,
+        inLevel: 1,
+      };
+      blocks.push(block);
     }
   }
   return blocks;
